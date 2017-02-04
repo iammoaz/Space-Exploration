28282019b48b0812ec342213dd8509c95619236e
Muhammad Moaz <m-moaz@hotmail.com>
2017-02-04 18:45:30 +0300

    Add helper methods to page model

--- Alien Life/Page.swift
+++ Alien Life/Page.swift
@@ -21,3 +21,21 @@ class Page {
         self.story = story
     }
 }
+
+extension Page {
+    
+    func addChoiceWith(title: String, story: Story) -> Page {
+        let page = Page(story: story)
+        return addChoiceWith(title: title, page: page)
+    }
+    
+    func addChoiceWith(title: String, page: Page) -> Page {
+        switch (firstChoice, secondChoice) {
+            case (.some, .some): return self
+            case (.none, .none), (.none, .some): firstChoice = (title, page)
+            case (.some, .none): secondChoice = (title, page)
+        }
+        
+        return page
+    }
+}
