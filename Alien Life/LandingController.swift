6b1e8aa49fe88e80977b45996771b367d7a80c71
Muhammad Moaz <m-moaz@hotmail.com>
2017-02-05 11:53:53 +0300

    Add name text field and related delegate methods

--- Alien Life/LandingController.swift
+++ Alien Life/LandingController.swift
@@ -9,10 +9,18 @@
 import UIKit
 
 class LandingController: UIViewController {
+    
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var nameTextFieldBottomConstraint: NSLayoutConstraint!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        nameTextField.delegate = self
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(LandingController.keyboardWillShow(_:)), name: Notification.Name.UIKeyboardWillShow, object: nil)
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(LandingController.keyboardWillHide(_:)), name: Notification.Name.UIKeyboardWillHide, object: nil)
     }
 
     override func didReceiveMemoryWarning() {
@@ -22,11 +30,41 @@ class LandingController: UIViewController {
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "PageController" {
+            guard let name = nameTextField.text, name != "" else { return }
             guard let pageController = segue.destination as? PageController else { return }
-            pageController.page = Adventure.story
+            pageController.page = Adventure.story(withName: name)
         }
     }
+    
+    func keyboardWillShow(_ notification: Notification) {
+        guard let info = notification.userInfo, let keyboardFrame = info[UIKeyboardFrameEndUserInfoKey] as? NSValue else { return }
+        
+        let frame = keyboardFrame.cgRectValue
+        nameTextFieldBottomConstraint.constant = frame.size.height + 10
+        
+        UIView.animate(withDuration: 0.8) { 
+            self.view.layoutIfNeeded()
+        }
+    }
+    
+    func keyboardWillHide(_ notification: Notification) {
+        nameTextFieldBottomConstraint.constant = 80
+        
+        UIView.animate(withDuration: 0.8) {
+            self.view.layoutIfNeeded()
+        }
+    }
+    
+    deinit {
+        NotificationCenter.default.removeObserver(self)
+    }
 
+}
 
+extension LandingController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
 }
 
